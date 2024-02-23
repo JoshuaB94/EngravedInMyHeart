5d0b4a39b783cbf8d231c903a89e992d7f712650
Joshua Berrios <ixoyedesignstudio@gmail.com>
2024-02-23 15:16:26 -0600

    Recreated Vue Project + Installed Tailwind CSS

---
+++ engravedinmyheart/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+import { RouterView } from 'vue-router'
+<style scoped></style>
---
+++ engravedinmyheart/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold">Home</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
