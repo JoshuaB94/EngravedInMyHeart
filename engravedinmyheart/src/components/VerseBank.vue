85f48daf29a8ec89d143a3c30f0852323a90e23c
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2024-02-24 12:46:55 -0600

    Modified API development of the app to a more simple approach

--- engravedinmyheart/src/components/VerseBank.vue
+++ engravedinmyheart/src/components/VerseBank.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
     <h1>Verse Bank</h1>
-    <p></p>
+    {{ verses }}
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// @ts-ignore
+import authToken from '../../authToken'
+import { onMounted, ref } from 'vue'
+import { useFetch } from '@vueuse/core'
+
+const authenicationToken = authToken
+
+const verses = ref([])
+
+onMounted(async () => {
+  const { data: verseData }: any = await useFetch(
+    'https://api.baserow.io/api/database/rows/table/259126/?user_field_names=true',
+    {
+      headers: {
+        Authorization: `Token ${authenicationToken}`
+      }
+    }
+  )
+
+  verses.value = verseData
+  console.log(verseData)
+})
+</script>
 
 <style scoped></style>
