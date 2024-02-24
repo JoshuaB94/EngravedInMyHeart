cb1bf7282d1003335663fd1c94f32e1a66a91041
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2024-02-24 13:21:02 -0600

    API Implementation v1

--- engravedinmyheart/src/components/VerseBank.vue
+++ engravedinmyheart/src/components/VerseBank.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1>Verse Bank</h1>
-    {{ verses }}
+
+    <div v-if="error" class="bg-red-200 rounded p-5 w-[320px] text-red-800">{{ error }}</div>
+
+    <div v-else-if="isFetching" class="bg-green-200 rounded p-5 w-[320px] text-green-800">
+      Loading: {{ isFetching }}
+    </div>
+
+    <pre v-else class="w-[420px] p-5">{{ data }}</pre>
   </div>
 </template>
 
@@ -13,21 +20,21 @@ import { useFetch } from '@vueuse/core'
 
 const authenicationToken = authToken
 
-const verses = ref([])
+const verses = ref()
+const verseID = ref()
 
-onMounted(async () => {
-  const { data: verseData }: any = await useFetch(
-    'https://api.baserow.io/api/database/rows/table/259126/?user_field_names=true',
-    {
-      headers: {
-        Authorization: `Token ${authenicationToken}`
-      }
+const { isFetching, error, data } = useFetch(
+  'https://api.baserow.io/api/database/rows/table/259126/?user_field_names=true',
+  {
+    headers: {
+      Authorization: `Token ${authenicationToken}`
     }
-  )
+  }
+)
 
-  verses.value = verseData
-  console.log(verseData)
-})
+verses.value = data
+
+console.log(verses)
 </script>
 
 <style scoped></style>
